721e44d29f9643f9c999f609a55093775f4f41bb
lets5steady <lets.05.steady@gmail.com>
2026-07-08 19:10:11 +0900

    30_Menu類のリンク先追加
    MenuBar,Header,FixedMenu,DrawerMenuコンポーネントのページ遷移先を設定

--- app/_components/MenuBar/MenuBar.tsx
+++ app/_components/MenuBar/MenuBar.tsx
@@ -7,7 +7,7 @@ export default function MenuBar() {
             <ul className={styles.list}>
                 <li className={styles.item}><Link href="/about" className={styles.link}>About</Link></li>
                 <li className={styles.item}><Link href="/#product" className={styles.link}>Product</Link></li>
-                <li className={styles.item}><Link href="/" className={styles.link}>News</Link></li>
+                <li className={styles.item}><Link href="/news" className={styles.link}>News</Link></li>
                 <li className={styles.item}><Link href="/#access" className={styles.link}>Access</Link></li>
                 <li className={styles.item}><Link href="/" className={styles.link}>Contact</Link></li>
             </ul>
