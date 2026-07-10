09e838f5dd4e3c270b2a3f2099269523dc44d8d5
lets5steady <lets.05.steady@gmail.com>
2026-07-11 02:17:17 +0900

    41_ショッピングガイド詳細ページ作成
    Delivery,Legal,Paymentコンポーネント作成
    各page.tsxも追加
    フッターよりページに移動可能

---
+++ app/delivery/page.tsx
@@ -0,0 +1,11 @@
+import Delivery from "../_components/Delivery/Delivery";
+import MenuBar from "../_components/MenuBar/MenuBar";
+
+export default function page() {
+    return(
+        <>
+            <MenuBar />
+            <Delivery />
+        </>
+    );
+}
