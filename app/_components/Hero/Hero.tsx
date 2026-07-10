2fbd92f32cac285e3fa9dae2f31b6fcc9e5ce223
lets5steady <lets.05.steady@gmail.com>
2026-07-10 21:27:21 +0900

    39_Heroコンポーネントスタイル調整
    スライダーへ変更する前と同じスタイルになるように調整

--- app/_components/Hero/Hero.tsx
+++ app/_components/Hero/Hero.tsx
@@ -10,29 +10,35 @@ export default function Hero() {
 
 
     return(
-        <div className={`${styles.embla} ${styles.container}`} ref={emblaRef}>
+        <div className={styles.embla} ref={emblaRef}>
             <div className={styles.embla__container}>
-                <div className={`${styles.embla__slide} ${styles.imgbox}`}>
-                    <Link href='/#gift' aria-label='ギフト商品を見る'>
-                        <Image
-                            src='/images/hero-giftbox.jpg'
-                            alt='ギフトボックス'
-                            width={1100}
-                            height={619}
-                            loading="eager"
-                        />
-                    </Link>
+                <div className={styles.embla__slide}>
+                    <div className={styles.imgbox}>
+                        <Link href='/#gift' aria-label='ギフト商品を見る'>
+                            <Image
+                                className={styles.img}
+                                src='/images/hero-giftbox.jpg'
+                                alt='ギフトボックス'
+                                width={1100}
+                                height={619}
+                                loading="eager"
+                            />
+                        </Link>
+                    </div>
                 </div>
-                <div className={`${styles.embla__slide} ${styles.imgbox}`}>
-                    <Link href='/about' aria-label='aboutページへ移動'>
-                        <Image
-                            src='/images/hero-sable.jpg'
-                            alt='こだわりのサブレディアマン'
-                            width={1100}
-                            height={619}
-                            loading="eager"
-                        />
-                    </Link>
+                <div className={styles.embla__slide}>
+                    <div className={styles.imgbox}>
+                        <Link href='/about' aria-label='aboutページへ移動'>
+                            <Image
+                                className={styles.img}
+                                src='/images/hero-sable.jpg'
+                                alt='こだわりのサブレディアマン'
+                                width={1100}
+                                height={619}
+                                loading="eager"
+                            />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
